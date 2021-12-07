86cf10ca964ae8cd2d1336a9babd702d552be01c
AntoDipp <52698241+AntoDipp@users.noreply.github.com>
2021-12-07 22:28:09 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const { Discord, Client, Collection, MessageEmbed, Intents } = require('discord.js');
+const client = new Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_INVITES']});
+//const config = require('./config.json'); <=== Add this if you want to use a config.json file
+client.on('ready', () => {
+  console.log(`${client.user.tag} is ready`)
+})
+client.login(process.env.'put the name of your . env file with the bot token') //if you want to use a config.json file change process.env.token with config.token and remember that the token value in the config file must have the token name!
